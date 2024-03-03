5de2296ff85dfdcb7d5138e60b3369785646adde
Badiul Jamal <badiuljamal3208@gmail.com>
2024-03-03 21:13:17 +0500

    update authentication guard

--- src/auth/guard/authenticated.guard.ts
+++ src/auth/guard/authenticated.guard.ts
@@ -6,15 +6,18 @@ export class AuthenticatedGuard implements CanActivate {
   constructor(private authService: AuthService) {}
   async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    const userinDb = await this.authService.checkUser(request.user);
-    if (request.isAuthenticated() && userinDb) {
-      if ((userinDb.company === undefined && request.route.path == '/profile/complete') || userinDb.company) {
-        return request.isAuthenticated();
-      } else {
-        throw new HttpException('Complete Profile', HttpStatus.NO_CONTENT); // Customize the response code here
-      }
-    } else {
+    if(!request.isAuthenticated()){
       return false;
     }
+    const userinDb = await this.authService.checkUser(request.user);
+    if (!userinDb) {
+      return false;
+    }
+    if ((userinDb.company === undefined && request.route.path == '/profile/complete') || userinDb.company) {
+      return request.isAuthenticated();
+    } else {
+      throw new HttpException('Complete Profile', HttpStatus.NO_CONTENT); // Customize the response code here
+    }
+    
   }
 }
